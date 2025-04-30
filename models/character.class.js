af3baa07b136f5a3daed5f360d3fb05d9b442c57
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-04-30 20:35:33 +0200

    added js files and img file

---
+++ models/character.class.js
@@ -0,0 +1,8 @@
+//durch extends MovableObject wird die Klasse Chicken von der Klasse MovableObject abgeleitet. Das bedeutet, dass die Klasse Chicken alle Eigenschaften und Methoden der Klasse MovableObject erbt und zusätzlich eigene Eigenschaften und Methoden hinzufügen kann.
+class Character extends MovableObject {
+
+  jump() {
+
+  }
+
+}
